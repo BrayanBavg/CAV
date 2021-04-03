95588c0c8a424842418fafdfdf7777960d752f4b
BrayanBavg <BrayanBavg@users.noreply.github.com>
2021-04-03 13:06:27 -0600

    BASE

---
+++ app/src/main/java/colegio/app/cav/Home/LoginUsuario.java
@@ -0,0 +1,16 @@
+package colegio.app.cav.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import colegio.app.cav.R;
+
+public class LoginUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_usuario);
+    }
+}
---
+++ app/src/main/java/colegio/app/cav/Home/Secundaria.java
@@ -0,0 +1,16 @@
+package colegio.app.cav.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import colegio.app.cav.R;
+
+public class Secundaria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secundaria);
+    }
+}
