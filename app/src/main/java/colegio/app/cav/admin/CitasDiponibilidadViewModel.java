a037c0875f64f0d7a62a039ae765372bd06be688
BrayanBavg <BrayanBavg@users.noreply.github.com>
2021-04-05 13:22:24 -0500

    Bavg-5-4-21

---
+++ app/src/main/java/colegio/app/cav/admin/CitasDiponibilidadViewModel.java
@@ -0,0 +1,18 @@
+package colegio.app.cav.admin;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class CitasDiponibilidadViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public CitasDiponibilidadViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Crud fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
